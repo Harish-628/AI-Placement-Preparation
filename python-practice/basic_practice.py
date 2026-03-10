e71d58e0071964e2409e7e3e0fb3bfec327118f8
Harish <harishsenthilraja628@gmail.com>
2026-03-10 23:37:27 +0530

    added the basic practice program (even / odd , largest number , sum of n)

--- python-practice/basic_practice.py
+++ python-practice/basic_practice.py
@@ -1,1 +1,21 @@
+#program 1 : even or odd
+def even_or_odd(n):
+  if n%2==0:
+   return "even"
+  else :
+   return "odd"
+print("Even or odd:",even_or_odd(9))
 
+#program 2 : Find largest numbers
+def find_largest(a,b,c):
+  return max(a,b,c)
+print("the largest number:",find_largest(3,6,8))
+
+#program 3 : Sum of the numbers from 1 to n
+def sum(n):
+  for i in range (1 , n+1):
+    total+ = i
+  return total 
+print("The sum of the numbers from 1 to n is ",sum(n))
+
+    
