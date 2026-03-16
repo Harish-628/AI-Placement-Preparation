5e4583bc6a2e2165cfd9d3e2109cb10e17344b63
Harish <harishsenthilraja628@gmail.com>
2026-03-16 23:27:58 +0530

    Create time_conversion.py

---
+++ dsa-problems/time_conversion.py
@@ -0,0 +1,17 @@
+#Time conversion on HackerRank
+def timeConversion(s):
+    hour = int(s[:2])
+    period = s[-2:]
+    rest = s[2:8]
+
+    if period == "AM":
+        if hour == 12:
+            hour = 0
+    else:  # PM
+        if hour != 12:
+            hour += 12
+
+    return f"{hour:02d}{rest}"
+
+s = input().strip()
+print(timeConversion(s))
