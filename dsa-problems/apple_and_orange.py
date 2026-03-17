56fdc05b099b1cd29790d1b6974c98ebdbd18051
Harish <harishsenthilraja628@gmail.com>
2026-03-17 23:39:14 +0530

    completed the apple and orange problem in HackerRank

---
+++ dsa-problems/apple_and_orange.py
@@ -0,0 +1,23 @@
+#The apple and orange problem in HackerRank
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    apple_count = 0
+    orange_count = 0
+
+    for apple in apples:
+        if s <= a + apple <= t:
+            apple_count += 1
+
+    for orange in oranges:
+        if s <= b + orange <= t:
+            orange_count += 1
+
+    print(apple_count)
+    print(orange_count)
+
+if __name__ == '__main__':
+    s, t = map(int, input().split())
+    a, b = map(int, input().split())
+    m, n = map(int, input().split())
+    apples = list(map(int, input().split()))
+    oranges = list(map(int, input().split()))
+    countApplesAndOranges(s, t, a, b, apples, oranges)
